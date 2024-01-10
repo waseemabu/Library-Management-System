10eac359220d989ee8928a90b203c561f3a86347
waseemabu <95966507+waseemabu@users.noreply.github.com>
2024-01-10 09:03:54 +0300

    Add files via upload

---
+++ src/main/java/library/librarymanagmementsystem/LibraryManagmementSystemApplication.java
@@ -0,0 +1,13 @@
+package library.librarymanagmementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagmementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagmementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/library/librarymanagmementsystem/LibraryManagmementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package library.librarymanagmementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagmementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
